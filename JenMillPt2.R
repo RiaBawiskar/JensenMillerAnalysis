59d74b2f25818247f21ab1d9f220232adbb9e7a1
RiaBawiskar <38380844+RiaBawiskar@users.noreply.github.com>
2023-12-17 00:53:28 -0600

    Update JenMillPt2.R

--- JenMillPt2.R
+++ JenMillPt2.R
@@ -1,8 +1,8 @@
 install.packages('haven')
 library(haven)
 df <-read_dta("Giffen.dta")
-df_hunan <- subset(df,(province == 'Hunan'))
-df_gansu <- subset(df,(province == 'Gansu'))
+df_hunan <- subset(df,(province == 'Hunan') & (person_id == min_p))
+df_gansu <- subset(df,(province == 'Gansu') & (person_id == min_p))
 
 #hunan indep var: percent change in rice price per household
 perc_change_rice_price <- as.vector(df_hunan$pct_ch_sub_rice_arc)
@@ -20,10 +20,14 @@ perc_change_wheat_price <- na.omit(perc_change_wheat_price)
 perc_change_wheat_cons <- as.vector(df_gansu$pct_ch_hh_wheat)
 perc_change_wheat_cons <- na.omit(perc_change_wheat_cons)
 
+#pad shortest vectors with NA's to have same length as longest vectors
+length(perc_change_rice_price) <- length(perc_change_wheat_price)
+length(perc_change_rice_cons) <- length(perc_change_wheat_cons)
+
 calculation_df <- data.frame(perc_change_rice_price,perc_change_rice_cons,
                              perc_change_wheat_price, perc_change_wheat_cons)
 colnames(calculation_df) <- c('p_change_rice_price','p_change_rice_consumption',
                               'p_change_wheat_price','p_change_wheat_consumption')
 
-hunan_slr <- lm(p_change_rice_price ~ p_change_rice_consumption, data=calculation_df)
-gansu_slr <- lm(p_change_wheat_price ~ p_change_wheat_consumption, data=calculation_df)
+hunan_slr <- lm(p_change_rice_consumption ~ p_change_rice_price, data=calculation_df)
+gansu_slr <- lm(p_change_wheat_consumption ~ p_change_wheat_price, data=calculation_df)
